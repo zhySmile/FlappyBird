8c5770fd782dd4c7fcb58c09864ccb3acd5fa3d3
zhanghaiyan <1135744872@qq.com>
2018-06-13 00:26:26 +0800

    UI：添加界面，添加管道移动逻辑

---
+++ UnityProject/FlappyBird/Assets/Script/Channel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Channel : MonoBehaviour
+{
+    public void SetChannel(Channel last, Channel next)
+    {
+        _lastChannel = last;
+        _nextChannel = next;
+    }
+
+    void Update()
+    {
+        this.GetComponent<RectTransform>().localPosition += new Vector3(1, 0, 0) * _speed;
+        if (_isActive && this.GetComponent<RectTransform>().localPosition.x < -1640f)
+        {
+            _isActive = false;
+            ResetPosition();
+        }
+    }
+
+    private void ResetPosition()
+    {
+        this.GetComponent<RectTransform>().localPosition = new Vector3
+            (_lastChannel.gameObject.GetComponent<RectTransform>().localPosition.x + _distance,
+            this.GetComponent<RectTransform>().localPosition.y,
+            this.GetComponent<RectTransform>().localPosition.z);
+        SetPositionY();
+        _isActive = true;
+    }
+
+    private void SetPositionY()
+    {
+        float y = Random.Range(-370f, 720f);
+        this.GetComponent<RectTransform>().localPosition = new Vector3
+           (this.GetComponent<RectTransform>().localPosition.x, y,
+           this.GetComponent<RectTransform>().localPosition.z);
+    }
+
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _distance;
+
+    private Channel _lastChannel;
+    private Channel _nextChannel;
+
+    private bool _isActive = true;
+}
---
+++ UnityProject/FlappyBird/Assets/Script/TriggerPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPanel : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < _channels.Count; i++)
+        {
+            if (i == 0)
+            {
+                _channels[i].SetChannel(_channels[_channels.Count - 1], _channels[i + 1]);
+            }
+            else if (i == _channels.Count - 1)
+            {
+                _channels[i].SetChannel(_channels[i - 1], _channels[0]);
+            }
+            else
+            {
+                _channels[i].SetChannel(_channels[i - 1], _channels[i + 1]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    [SerializeField]
+    private List<Channel> _channels;
+}
