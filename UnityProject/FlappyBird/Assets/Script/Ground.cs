67ae44b39883221b5a40864da0c055463ffe2e73
zhanghaiyan <1135744872@qq.com>
2018-06-13 23:01:45 +0800

    管道：添加地板移动

--- UnityProject/FlappyBird/Assets/Script/Ground.cs
+++ UnityProject/FlappyBird/Assets/Script/Ground.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ground : MonoBehaviour {
+public class Ground : MonoBehaviour
+{
+    void Update()
+    {
+        this.GetComponent<RectTransform>().localPosition += new Vector3(1, 0, 0) * _speed;
+        if (this.GetComponent<RectTransform>().localPosition.x <
+            -this.GetComponent<RectTransform>().sizeDelta.x)
+        {
+            ResetPosition();
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void ResetPosition()
+    {
+        this.GetComponent<RectTransform>().localPosition = new Vector3
+            (this.gameObject.GetComponent<RectTransform>().localPosition.x
+            + 2 * (this.GetComponent<RectTransform>().sizeDelta.x - 3f),
+            this.GetComponent<RectTransform>().localPosition.y,
+            this.GetComponent<RectTransform>().localPosition.z);
+    }
+
+
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private Ground _ground;
+
+    private Channel _lastChannel;
+    private Channel _nextChannel;
 }
